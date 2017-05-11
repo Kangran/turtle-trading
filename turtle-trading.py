d68d6b8b38d3c401d82a2fb798cd5b3395c0b486
vyq <qvc246+github@gmail.com>
2017-05-11 12:17:16 +0800

    Refactor

--- turtle-trading.py
+++ turtle-trading.py
@@ -6,24 +6,29 @@ def initialize(context):
     Set up algorithm.
     """
     # https://www.quantopian.com/help#available-futures
-    context.markets = [
-        continuous_future('US'),
-        continuous_future('TY'),
-        continuous_future('SB'),
-        continuous_future('SF'),
-        continuous_future('BP'),
-        continuous_future('JY'),
-        continuous_future('CD'),
-        continuous_future('SP'),
-        continuous_future('ED'),
-        continuous_future('TB'),
-        continuous_future('GC'),
-        continuous_future('SV'),
-        continuous_future('HG'),
-        continuous_future('CL'),
-        continuous_future('HO'),
-        continuous_future('HU')
+    markets = [
+        'US',
+        'TY',
+        'SB',
+        'SF',
+        'BP',
+        'JY',
+        'CD',
+        'SP',
+        'ED',
+        'TB',
+        'GC',
+        'SV',
+        'HG',
+        'CL',
+        'HO',
+        'HU'
     ]
+    
+    context.markets = map(
+        lambda market: continuous_future(market),
+        markets
+    )
     
     assert(len(context.markets) == 16)
 
